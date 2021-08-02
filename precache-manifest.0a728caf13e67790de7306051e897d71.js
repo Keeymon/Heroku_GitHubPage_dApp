ab03ea292c01ba6561d940fab769731505173d08
Yann Pichereau <yann_pichereau@hotmail.fr>
2021-08-02 17:37:16 +0200

    Updates

---
+++ precache-manifest.0a728caf13e67790de7306051e897d71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd4469433083aa6795d9302b00255d6",
+    "url": "/Heroku_GitHubPage_dApp/index.html"
+  },
+  {
+    "revision": "b43b1463c1298d9eeb27",
+    "url": "/Heroku_GitHubPage_dApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0ea6b258f7601bfbc927",
+    "url": "/Heroku_GitHubPage_dApp/static/js/2.f7504f16.chunk.js"
+  },
+  {
+    "revision": "b43b1463c1298d9eeb27",
+    "url": "/Heroku_GitHubPage_dApp/static/js/main.ebcb5d4d.chunk.js"
+  },
+  {
+    "revision": "d2fdf8a682b9f46f4d72",
+    "url": "/Heroku_GitHubPage_dApp/static/js/runtime-main.1c6b297a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Heroku_GitHubPage_dApp/precache-manifest.0a728caf13e67790de7306051e897d71.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Heroku_GitHubPage_dApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
